1536a18b1d574ce21bf5b9bc17bd871ac5099409
Marinho3104 <mariovieira2003@gmail.com>
2022-12-07 11:04:37 +0000

    Redo everything

--- built_in/built_in_definitions.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#ifndef BUILT_IN_DEFINITIONS_H
-#define BUILT_IN_DEFINITIONS_H
-
-#define BUILT_IN_TYPE_SIZE_BYTE 1
-
-#endif
--- byteCode/byteCode.cpp
+++
@@ -1,6 +0,0 @@
-#include "./byteCode.h"
-
-#include <iostream>
-
-byte_code::Byte_Code::Byte_Code(unsigned char _code, int _arg) : code(_code), argument(_arg) {}
-
